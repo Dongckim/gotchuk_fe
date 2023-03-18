8ff7bb01290aa75172dba84339fd1a7d17d06166
hojiii <shimn3060@naver.com>
2023-03-18 22:00:55 +0900

    메인 페이지(채팅시작 연결),로그인(토큰주석처리),회원가입(완료)

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import match from "../modules/match";
+import users from "../modules/login"
+import gameList from "../modules/main"
 
 const store = configureStore({
   reducer: { 
-    match,
+    match, users,gameList
 },
 devTools: process.env.NODE_ENV === "developmetns" ? false : true,
 });
