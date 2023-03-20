1ce2c2a495155699b90f66579b6e8f0ab5145455
hojiii <shimn3060@naver.com>
2023-03-21 02:01:26 +0900

    작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import MainHeader from "./components/MainComponents/MainHeader";
 import Router from "./shared/Router";
 
 function App() {
   return (
     <>
-      <MainHeader />
       <Router />
     </>
   );
